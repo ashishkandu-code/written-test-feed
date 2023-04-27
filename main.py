867ef99d3845fa7f571f01663034502295855686
Ashish Kandu <ashishkandu43@gmail.com>
2023-04-27 22:18:01 +0545

    improve mail format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from mailer import Mailer
 from fetcher import Fetcher
-from endpoints import news_events_endpoint
+from endpoints import news_events_endpoint, trial_forms_endpoint, written_forms_endpoint
 from mylogging import log_setup
 
 #set up logging configuration
@@ -12,15 +12,17 @@ logger = logging.getLogger(__name__)
 def main():
     fetch = Fetcher()
 
-    response = fetch.fetch_forms(news_events_endpoint)
-    contents = fetch.response_hanlder(response, news_events_endpoint)
+    response = fetch.fetch_forms(written_forms_endpoint)
+    contents = fetch.response_hanlder(response, written_forms_endpoint)
     if contents:
         body = ''
+        if len(contents) == 5:
+            body += '\nLatest feeds:\n'
         for content in contents:
             title, download_fileUrl, formatted_date, endpoint_name = content
             body += f'\n{title} - {formatted_date}\nDownload PDF:\n{download_fileUrl}\n'
 
-        content = f'[{endpoint_name} Notice]\n{body}\nThanks,\nAshish Bot'.encode(
+        content = f'[{endpoint_name} notice]\n{body}\nThanks,\nAshish Bot'.encode(
             'utf-8')
 
         mailbot = Mailer()
